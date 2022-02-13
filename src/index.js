c753307c6518617d57ade3b37cbddc83545619a9
EvgeKat <katargina_evg@mail.ru>
2022-02-13 18:39:40 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -37,8 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const SYMBOLS = {
+    '00': '',
+    '10': '.',
+    '11': '-',
+    '**': ' ',
+}
+
 function decode(expr) {
-    // write your solution here
+    let result = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        result.push(expr.slice(i, i + 10));
+    }
+    result = result.map(elem => {
+    let pair = [];
+        for (let i = 0; i < elem.length; i += 2) {
+            pair.push(elem.slice(i, i + 2));
+        }
+        return pair;
+        })
+    .map(el => el.map(pair => SYMBOLS[pair]))
+    .map(el => el.join(''))
+    .map(el => {
+        if ( el == '     ') { 
+            return ' ';}
+        else {return MORSE_TABLE[el];}
+         })
+     .join('');
+       return  result;
 }
 
 module.exports = {
